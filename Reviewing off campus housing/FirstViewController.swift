2558cc78e57d2a9822f09b6420dd0b3e1be04123
Silin Chen <schen23@lcs-209-047.syr.edu>
2019-03-01 16:35:19 -0500

    Initial Commit

---
+++ Reviewing off campus housing/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Reviewing off campus housing
+//
+//  Created by Silin Chen on 3/1/19.
+//  Copyright © 2019 housing. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Reviewing off campus housing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Reviewing off campus housing
+//
+//  Created by Silin Chen on 3/1/19.
+//  Copyright © 2019 housing. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
