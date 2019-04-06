6196d06b64a9ab384405bc8ba3796c444584d6ae
Kevin James Fu <kfu101@lcs-209-165.syr.edu>
2019-04-05 21:39:58 -0400

    retrieved map values & print, writeReview broken?

--- Reviewing off campus housing/displayListingViewController.swift
+++ Reviewing off campus housing/displayListingViewController.swift
@@ -8,44 +8,100 @@
 
 import UIKit
 
-class displayListingViewController: UIViewController {
+class displayListingViewController: UIViewController { // , UITableViewDataSource, UITableViewDelegate
 
     // adding a comment here from kevin
-
+    
+    
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var AddressLabel: UILabel!
+    @IBOutlet weak var landlordLabel: UILabel!
+    @IBOutlet weak var rentPriceLabel: UILabel!
+    
+    
+    //@IBOutlet weak var reviewTable: UITableView!
+    
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        AddressLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
+        landlordLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
+        rentPriceLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
 
         // Do any additional setup after loading the view.
-        print("displayu")
-        print(info)
+        //print("displayu")
+        //print(info)
         
         
         let docRef = db.collection("listings").document(info)
 
         docRef.getDocument { (document, error) in
             if let document = document, document.exists {
+                
                 let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
                 //print("Document data: \(dataDescription)")
+                
                 let address = document.get("address") ?? ""
-                let rent = (document.get("rent")) //?? 0
-                let landlordName = document.get("landlordName")
-                let review = document.get("reviews") ?? ""
-                print(review)
-                self.label.text = (address as! String)
-                //print(address)
-                
-                self.label2.text = (landlordName as! String)
-                //print(landlordName)
-                
-                //self.label3.text = rent
+                let rent = (document.get("rent"))
+                let landlordName = document.get("landlordName") ?? ""
+                let review = document.get("reviews") as! NSDictionary //NSMapTable<AnyObject, AnyObject>
+                // currently this gives a fatal error when a house w/ no reviews is clicked,
+                // but we should make sure that every listing comes with an NSDictionary (aka review map)
+                // when created.
                 
                 
-                self.mk = address as! String
+                //for (key,val) in review {
+                //    print(key)
+                //    print("\n\n")
+                //    let newVal = val as! NSDictionary
+                //    for (key2, val2) in newVal {
+                //        print(key2)
+                //        print(val2)
+                //    }
+                //}
+                
+                
+                // Retreiving values from map of maps (reviews)
+                
+                // make sure all fields in map are present and valid to present fatal errors.
+                
+                for (reviewer, reviewMap) in review {
+                    let reviewer = reviewer as! String
+                    print("reviewer: " + reviewer)
+                    
+                    let reviewMap = reviewMap as! NSDictionary
+                    let comments = reviewMap.value(forKey: "comments") as! String
+                    let rating = reviewMap.value(forKey: "rating") as! Int
+                    let isAnonymous = reviewMap.value(forKey: "isAnonymous") as! Bool
+                    let isEdited = reviewMap.value(forKey: "isEdited") as! Bool
+                    let willLiveAgain = reviewMap.value(forKey: "willLiveAgain") as! Bool
+                    
+                    print("comments: " + comments)
+                    print("rating: " + String(rating))
+                    print("isAnonymous: " + String(isAnonymous))
+                    print("isEdited: " + String(isEdited))
+                    print("willLiveAgain: " + String(willLiveAgain))
+                    
+                    print("\n")
+                }
+                
+                
+                
+                self.label.text = address as! String
+                self.label2.text = landlordName as! String
+ 
+                // TROUBLE: how to set label with an int? rent is an int, and swift is complaining.
+                //let newrent = rent as! String
+                //self.label3.text = newrent as! String
+                
+                
+                //self.mk = address as! String
             } else {
                 print("Document does not exist")
             }
@@ -82,6 +138,19 @@ class displayListingViewController: UIViewController {
             vc?.info = mk
         }
     }
+    
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 10 // data.count
+//
+//    }
+//
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        // cell.textLabel?.text = data[indexPath.row]
+//        return cell
+//    }
     
     /*
     // MARK: - Navigation
