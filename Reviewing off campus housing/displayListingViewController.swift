25b32ac6944b55d541ab05e5258dba04b18cdfc8
Kevin James Fu <kfu101@lcs-209-165.syr.edu>
2019-04-18 22:40:20 -0400

    still trying to figure out tableview

--- Reviewing off campus housing/displayListingViewController.swift
+++ Reviewing off campus housing/displayListingViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
-class displayListingViewController: UIViewController {
+class displayListingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     // adding a comment here from kevin
     
     var AverageRating:Double = 0
     var countReviews:Double = 0
+    
+    var reviewArr:[String] = ["sample"]
+    let testArr = ["this is a long string that i am typing to see if the string goes to the second line yay", "ghi"]
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var label2: UILabel!
@@ -25,6 +28,9 @@ class displayListingViewController: UIViewController {
     @IBOutlet weak var landlordLabel: UILabel!
     @IBOutlet weak var rentPriceLabel: UILabel!
     @IBOutlet weak var avgRating: UILabel!
+    
+    // reviewTable connection
+    @IBOutlet weak var reviewTable: UITableView!
     
     
     @IBOutlet weak var scrollReview: UIScrollView!
@@ -35,7 +41,6 @@ class displayListingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        
         //AddressLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
         //landlordLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
         //rentPriceLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
@@ -44,123 +49,87 @@ class displayListingViewController: UIViewController {
         //scrollReview.contentLayoutGuide.bottomAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
 
         // Do any additional setup after loading the view.
-
-        showReviews()
-        db.collection("listings").document(info)
-            .addSnapshotListener { documentSnapshot, error in
-                guard let document = documentSnapshot else {
-                    print("Error fetching document: \(error!)")
-                    return
-                }
-                guard let data = document.data() else {
-                    print("Document data was empty.")
-                    return
-                }
-                //print("Current data: \(data)")
-                self.showReviews()
-        }
-
-    } //  end of viewDidLoad
-    
-    var info:String = ""
-    var Em:String = ""
-    
-    func showReviews(){
-        let docRef = db.collection("listings").document(info)
         
+        print("initial string?")
+        
+        //showReviews()
+        
+//        db.collection("listings").document(info)
+//            .addSnapshotListener { documentSnapshot, error in
+//                guard let document = documentSnapshot else {
+//                    print("Error fetching document: \(error!)")
+//                    return
+//                }
+//                guard let data = document.data() else {
+//                    print("Document data was empty.")
+//                    return
+//                }
+//                //print("Current data: \(data)")
+//                self.showReviews()
+//        }
+        
+//        print("sleeping 2")
+//        sleep(2)
+        
+        let docRef = db.collection("listings").document(info)
         docRef.getDocument { (document, error) in
+            
             if let document = document, document.exists {
+                // i got rid of the majority of the code here because i wanted to test to see if it was the code in this
+                // section that was the problem or just the fact that we're utilizing getDocument. it turns out that
+                // getDocument is the culprit. it runs after the print statement print("sandwich"), regardless of code that is inside.
+                // the original code is at the bottom of this file. -kevin 4/18/2019
+ 
+                print("test print here")
+                var reviewString = "test" // current method of displaying reviews: a long chain of text
+                self.reviewArr.append(reviewString)
                 
-                //let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                //print("Document data: \(dataDescription)")
-                
-                let address = document.get("address") ?? ""
-                let rent = (document.get("rent"))
-                let landlordName = document.get("landlordName") ?? "Leasing manager unavailable"
-                
-                //COMMENT THIS SECTION OF CODE OUT UNTIL THE CODE FOR ADDING AN EMPTY MAP WHEN SUBMITTING A NEW LISTING IS ADDED
-                /////////////////////////////////////////////////////////////////////////////////////////////////
-                
-                let review = document.get("reviews") as! NSDictionary
-                
-                //currently this gives a fatal error when a house w/ no reviews is clicked,
-                //but we should make sure that every listing comes with an NSDictionary (aka review map)
-                //when created.
-                
-                //Retreiving values from map of maps (reviews)
-                //Also! make sure all fields in map are present and valid to present fatal errors. (like comments, rating, isAnon, etc all must be present)
-                
-                var reviewString = "" // current method of displaying reviews: a long chain of text
-                
-                for (reviewer, reviewMap) in review {
-                    let reviewer = reviewer as! String
-                    //print("reviewer: " + reviewer)
-                    
-                    let reviewMap = reviewMap as! NSDictionary
-                    
-                    if reviewMap.count == 0 { // temporary fix. doesn't fix if some fields are missing, I think.
-                        reviewString = "No review information for " + reviewer
-                        break
-                    }
-                    
-                    let comments = reviewMap.value(forKey: "comments") as! String
-                    let rating = reviewMap.value(forKey: "rating") as! Float
-                    self.AverageRating = self.AverageRating + Double(rating)
-                    self.countReviews = self.countReviews + 1
-                    let isAnonymous = reviewMap.value(forKey: "isAnonymous") as! Bool
-                    let isEdited = reviewMap.value(forKey: "isEdited") as! Bool
-                    let willLiveAgain = reviewMap.value(forKey: "willLiveAgain") as! Bool
-                    
-                    print("comments: " + comments)
-                    print("rating: " + String(rating))
-                    print("isAnonymous: " + String(isAnonymous))
-                    print("isEdited: " + String(isEdited))
-                    print("willLiveAgain: " + String(willLiveAgain))
-                    
-                    print("\n")
-                    
-                    if isAnonymous == false {
-                        reviewString = reviewString + "Reviewer: " + String(reviewer) + "\n"
-                    }
-                    reviewString = reviewString + "Rating: " + String(format: "%.1f",rating) + "\n"
-                    reviewString = reviewString + "Comments: \n" + comments + "\n"
-                    reviewString = reviewString + "Would live again? " + (willLiveAgain ? "Yes" : "No")
-                    reviewString = reviewString + "\n\n"
-                }
-                
-                if reviewString == "" { reviewString = "There are no reviews." }
-                self.reviewText.text = reviewString
-                
-                // IMPORTANT NOTE TO SELF
-                // need to: for each review, put it in an array as its own element.
-                // then, use a tableView, iterate on the array, and show each element as its own cell.
-                
-                /////////////////////////////////////////////////////////////////////////////////////////////////
-                //END COMMENT BLOCK
-                
-                self.label.text = (address as! String) // label for address
-                self.label2.text = (landlordName as! String) // label for landlord
-                
-                //if (rent != nil)  { self.label3.text = String(format: "%@", rent as! CVarArg) } // label for rent
-                //else              { self.label3.text = "No rent information" } // case of nothing
-                
-                //self.mk = address as! String
+                print("call from showReviews")
+                print(self.reviewArr)
             }
                 
             else {
                 print("Document does not exist")
             }
             
-            if (self.countReviews != 0) {
-                let avgrate = (self.AverageRating/self.countReviews)
-                self.avgRating.text = String(format: "%.1f", avgrate) }
         }
+
+        print("call from viewDidLoad")
+        //print(self.reviewArr)
+        print(reviewArr)
+        print("sandwich")
         
+        //reviewTable.reloadData()
+
+    } //  end of viewDidLoad
+    
+    var info:String = ""
+    var Em:String = ""
+    
+    //func showReviews(){
         
+    //}
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("count:" + String(self.reviewArr.count))
+        print("reviewArr:")
+        print(self.reviewArr)
+        return reviewArr.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("HELLO")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        let text = reviewArr[indexPath.row]
+        cell?.textLabel?.text = text
         
-        
-        
-        
+        return cell!
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
     @IBAction func deleteReview(_ sender: UIButton) {
@@ -251,3 +220,118 @@ class displayListingViewController: UIViewController {
     */
 
 }
+
+
+
+
+/////////////
+//// algorithm for database
+//func showReviews(){
+//    let docRef = db.collection("listings").document(info)
+//
+//    docRef.getDocument { (document, error) in
+//        if let document = document, document.exists {
+//
+//            //let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+//            //print("Document data: \(dataDescription)")
+//
+//            let address = document.get("address") ?? ""
+//            let rent = (document.get("rent"))
+//            let landlordName = document.get("landlordName") ?? "Leasing manager unavailable"
+//
+//            //COMMENT THIS SECTION OF CODE OUT UNTIL THE CODE FOR ADDING AN EMPTY MAP WHEN SUBMITTING A NEW LISTING IS ADDED
+//            /////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//            let review = document.get("reviews") as! NSDictionary
+//
+//            //currently this gives a fatal error when a house w/ no reviews is clicked,
+//            //but we should make sure that every listing comes with an NSDictionary (aka review map)
+//            //when created.
+//
+//            //Retreiving values from map of maps (reviews)
+//            //Also! make sure all fields in map are present and valid to present fatal errors. (like comments, rating, isAnon, etc all must be present)
+//
+//            var reviewString = "" // current method of displaying reviews: a long chain of text
+//
+//            for (reviewer, reviewMap) in review {
+//                let reviewer = reviewer as! String
+//                //print("reviewer: " + reviewer)
+//
+//                let reviewMap = reviewMap as! NSDictionary
+//
+//                //if reviewMap.count == 0 { // temporary fix. doesn't fix if some fields are missing, I think.
+//                //    reviewString = "No review information for " + reviewer
+//                //    break
+//                //}
+//
+//                let comments = reviewMap.value(forKey: "comments") as! String
+//                let rating = reviewMap.value(forKey: "rating") as! Float
+//                self.AverageRating = self.AverageRating + Double(rating)
+//                self.countReviews = self.countReviews + 1
+//                let isAnonymous = reviewMap.value(forKey: "isAnonymous") as! Bool
+//                let isEdited = reviewMap.value(forKey: "isEdited") as! Bool
+//                let willLiveAgain = reviewMap.value(forKey: "willLiveAgain") as! Bool
+//
+//                print("comments: " + comments)
+//                print("rating: " + String(rating))
+//                print("isAnonymous: " + String(isAnonymous))
+//                print("isEdited: " + String(isEdited))
+//                print("willLiveAgain: " + String(willLiveAgain))
+//
+//                print("\n")
+//
+//                if isAnonymous == false {
+//                    reviewString = reviewString + "Reviewer: " + String(reviewer) + "\n"
+//                }
+//                reviewString = reviewString + "Rating: " + String(format: "%.1f",rating) + "\n"
+//                reviewString = reviewString + "Comments: \n" + comments + "\n"
+//                reviewString = reviewString + "Would live again? " + (willLiveAgain ? "Yes" : "No")
+//                reviewString = reviewString + "\n\n"
+//
+//                self.reviewArr.append(reviewString)
+//            } // end for loop
+//
+//            print("test here")
+//            print(self.reviewArr)
+//
+//            print("call from showReviews")
+//            print(self.reviewArr)
+//
+//            print("hi")
+//            print(self.reviewArr)
+//            reviewTable.dataSource = self
+//
+//
+//            //if reviewString == "" { reviewString = "There are no reviews." }
+//            //self.reviewText.text = reviewString
+//
+//            // IMPORTANT NOTE TO SELF
+//            // need to: for each review, put it in an array as its own element.
+//            // then, use a tableView, iterate on the array, and show each element as its own cell.
+//
+//            /////////////////////////////////////////////////////////////////////////////////////////////////
+//            //END COMMENT BLOCK
+//
+//            self.label.text = (address as! String) // label for address
+//            self.label2.text = (landlordName as! String) // label for landlord
+//
+//            //if (rent != nil)  { self.label3.text = String(format: "%@", rent as! CVarArg) } // label for rent
+//            //else              { self.label3.text = "No rent information" } // case of nothing
+//
+//            //self.mk = address as! String
+//        } // end if document exists
+//
+//        else {
+//            print("Document does not exist")
+//        } // end else
+//
+//        if (self.countReviews != 0) {
+//            let avgrate = (self.AverageRating/self.countReviews)
+//            self.avgRating.text = String(format: "%.1f", avgrate) } // end if
+//
+//    } // end docRef.getDocument
+//
+//} // end showReviews
+//
+//
+//
